2a0a9ebf1d2e643ea8fc2a711f073c7bf118764f
Joe Micallef <jmicalle@gmail.com>
2017-10-22 08:07:00 -0700

    here is the js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+/*image selector*/
+
 var DETAIL_IMAGE_SELECTOR = `[data-image-role="target"]`;
 var DETAIL_TITLE_SELECTOR = `[data-image-role="title"]`;
 var THUMBNAIL_LINK_SELECTOR = `[data-image-role="trigger"]`;
